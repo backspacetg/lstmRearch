4dd1d4f57c5b3bafeaaee19d33cd3c1e5ffb9047
backspacetg <37428522+backspacetg@users.noreply.github.com>
2019-01-25 18:18:55 +0800

    Add files via upload

---
+++ lstmTest.py
@@ -0,0 +1,62 @@
+import math
+import torch as t
+import torch.nn as nn
+from torch.autograd import Function
+
+class Sigmoid(Function):
+    #缩放系数初步设置为0.5
+    scale = 1.5
+    @staticmethod
+    def forward(ctx,x):
+        output = 1 / (1 + t.exp(-x))
+        ctx.save_for_backward(output)
+        return output
+    @staticmethod
+    def backward(ctx, grad_output):
+        output, = ctx.saved_tensors
+        gradx = scale * output * (1-output) * grad_output
+        return gradx
+
+class Tanh(Function):
+    scale = 1.5
+    @staticmethod
+    def forward(ctx, x):
+        paraP = t.exp(x)
+        paraN = t.exp(-x)
+        output = (paraP-paraN)/(paraP+paraN)
+        ctx.save_for_backward(output)
+        return output
+    @staticmethod
+    def backward(ctx, grad_output):
+        output, = ctx.saved_tensors
+        gradx = scale * (1-output*output) * grad_output
+        return gradx
+
+class LstmCell(nn.Module):
+    def __init__(self,inSize,hideSize):
+        self.inputSize = inSize
+        self.hiddenSize = hideSize
+
+        self.combinedLinear = nn.Linear(inSize+hideSize,4*hideSize)
+
+        stdv = 1/math.sqrt(hideSize)
+        for para in self.parameters():
+            para.data.uniform_(-stdv,stdv)
+    def forward(self,x,h,c):
+        combinedInput = t.cat((x,h),0)
+        combinedOutput = self.combinedLinear(combinedInput)
+        forgetPart = combinedOutput[0:self.hiddenSize]
+        inputPart = combinedOutput[self.hiddenSize:2*self.hiddenSize]
+        cellPart = combinedOutput[2*self.hiddenSize:3*self.hiddenSize]
+        outputPart = combinedOutput[3*self.hiddenSize:4*self.hiddenSize]
+
+        newCellState = Sigmoid.apply(forgetPart) * c + Sigmoid.apply(inputPart) * Tanh.apply(cellPart)
+        newHiddenState = Sigmoid.apply(outputPart) * Tanh.apply(newCellState)
+
+        return newHiddenState,newCellState
+
+
+
+
+
+
